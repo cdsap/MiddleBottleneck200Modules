9015c702b75932b0cf3e85c980eac5d6f120c4a5
Inaki Villar <inaki.seri@gmail.com>
2023-11-17 15:18:15 -0600

    adding junit vintage and processes info

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,18 @@
 plugins {
     id("com.autonomousapps.dependency-analysis") version "1.21.0"
-    id("org.jetbrains.kotlin.jvm") version("1.9.10") apply false
+    id("org.jetbrains.kotlin.jvm") version ("1.9.10") apply false
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
+    id("io.github.cdsap.kotlinprocess") version "0.1.5"
+    id("io.github.cdsap.gradleprocess") version "0.1.1"
+    id("io.github.cdsap.testprocess") version "0.1.3"
+}
+
+allprojects {
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+        predictiveSelection {
+            enabled = true
+        }
+    }
 }
