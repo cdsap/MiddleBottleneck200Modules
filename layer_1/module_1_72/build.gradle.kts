4da23f75c5a282db2c8d3cb9be8c9caf193a794c
Inaki Villar <inaki.seri@gmail.com>
2023-12-01 16:10:19 +0100

    A

--- layer_1/module_1_72/build.gradle.kts
+++ layer_1/module_1_72/build.gradle.kts
@@ -1,10 +1,23 @@
 plugins {
   id("awesome.androidlib.plugin")
 }
-
+android {
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+}
 dependencies {
     implementation("com.google.auto.value:auto-value-annotations:1.10")
     kapt("com.google.auto.value:auto-value:1.6.6")
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
     implementation(project(":layer_0:module_0_24"))
     implementation(project(":layer_0:module_0_7"))
     implementation(project(":layer_0:module_0_25"))
