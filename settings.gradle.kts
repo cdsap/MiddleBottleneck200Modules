fc20a1061128ab476f0008f43ce8ee9cc74a94ca
Iñaki Villar <inaki.seri@gmail.com>
2023-12-06 17:04:35 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 plugins {
     id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
-    id("com.gradle.enterprise") version "3.15.1"
+    id("com.gradle.enterprise") version "3.16"
 }
 gradleEnterprise {
     server = "http://ge.solutions-team.gradle.com"
