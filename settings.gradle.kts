f9fcbeff2283d32bb57b1ba5b37f454bce81925d
Inaki Villar <inaki.seri@gmail.com>
2024-02-12 10:57:01 -0800

    update gradle 8.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 plugins {
     id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
-    id("com.gradle.enterprise") version "3.16"
+    id("com.gradle.enterprise") version "3.16.2"
 }
 gradleEnterprise {
     server = "http://ge.solutions-team.gradle.com"
