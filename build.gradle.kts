47d726894bb25e0577bbc79bdf4887f7efee1051
Inaki Villar <inaki.seri@gmail.com>
2025-08-26 11:23:09 -0700

    file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 
 plugins {
-    id("com.autonomousapps.dependency-analysis") version "1.21.0"
-    id("org.jetbrains.kotlin.jvm") version ("1.9.10") apply false
+    id("org.jetbrains.kotlin.jvm") version ("2.1.0") apply false
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
-    id("io.github.cdsap.kotlinprocess") version "0.1.5"
-    id("io.github.cdsap.gradleprocess") version "0.1.1"
+    id("io.github.cdsap.kotlinprocess") version "0.2.0"
+    id("io.github.cdsap.gradleprocess") version "0.2.0"
     id("io.github.cdsap.testprocess") version "0.1.3"
 }
 
 allprojects {
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
-        predictiveSelection {
+        develocity.predictiveSelection {
             enabled = true
         }
     }
